0b24c10e751cb6d13b50ed6383ed1367ebe19950
Kaviya <146524941+yavika123@users.noreply.github.com>
2025-04-01 23:23:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let countdown;
+const startBtn=document.querySelector('.start-btn');
+const stopBtn=document.querySelector('.stop-btn');
+const minutesInput = document.querySelector('.minutes');
+const secondsInput=document.querySelector('.seconds');
+const timerDisplay = document.querySelector('.timer');
+startBtn.addEventListener('click',startCountdown);
+stopBtn.addEventListener('click', stopCountdown);
+function startCountdown(){
+    let min=minutesInput.value ;Imnnn
+    let sec=secondsInput.value ;
+    let totalTime=min*60+parseInt(sec);
+    countdown=set
+}
+
+
+
+
