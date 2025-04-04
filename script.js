46650b8850167e330ea0f1bc89a31a93d6698048
Kaviya <146524941+yavika123@users.noreply.github.com>
2025-04-04 11:32:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,23 @@ const timerDisplay = document.querySelector('.timer');
 startBtn.addEventListener('click',startCountdown);
 stopBtn.addEventListener('click', stopCountdown);
 function startCountdown(){
-    let min=minutesInput.value ;Imnnn
-    let sec=secondsInput.value ;
+    clearInterval(countdown);
+    let min=minutesInput.value ||0;
+    let sec=secondsInput.value ||0;
     let totalTime=min*60+parseInt(sec);
-    countdown=set
+    parseInt(sec);
+    countdown=setInterval(()=>
+    {
+        let minutes=Math.floor(totalTime/60);
+        let seconds=totalTime%60;
+        timerDisplay.textContent=`${minutes.toString().padStart(2,'0')}:${seconds.toString().padStart(2,'0')}`;
+        if(totalTime<=0){
+            clearInterval(countdown);
+            timerDisplay.textContent="Time's up!!";alert("Time's up!");
+        }
+        totalTime--;
+    },1000)
 }
-
-
-
-
+function stopCountdown(){
+    clearInterval(countdown)
+}
